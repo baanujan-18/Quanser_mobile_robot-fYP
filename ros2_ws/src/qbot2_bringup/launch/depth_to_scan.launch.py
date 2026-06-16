9a3041f0b2071223530dd8f08487db51d96e2e8d
Baanujan <baanujannavaratnam@gmail.com>
2026-06-17 00:35:05 +0530

    Add depth to laser scan launch file

--- ros2_ws/src/qbot2_bringup/launch/depth_to_scan.launch.py
+++ ros2_ws/src/qbot2_bringup/launch/depth_to_scan.launch.py
@@ -1,1 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='depthimage_to_laserscan',
+            executable='depthimage_to_laserscan_node',
+            name='depthimage_to_laserscan',
+            remappings=[
+                ('depth', '/kinect/depth/image_raw'),
+                ('depth_camera_info', '/kinect/depth/camera_info_fixed'),
+                ('scan', '/scan'),
+            ],
+            parameters=[{
+                'scan_height': 10,
+                'range_min': 0.45,
+                'range_max': 4.0,
+                'output_frame': 'kinect_depth_frame',
+            }]
+        )
+    ])
